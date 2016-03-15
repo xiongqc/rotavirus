5e565be9f9176916dca2d7637443de64c5f6ac57
quickchase <qiancheng.xiong@gmail.com>
2016-03-15 23:55:45 +0000

    updated results for reporting

---
+++ 10 Detection of Positive Selection/MEME detections/VP4C_meme.php
@@ -0,0 +1,448 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+		<META http-equiv="Content-Style-Type" content="text/css">
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<title>
+			MEME
+		</title>		
+		<LINK REL=STYLESHEET TYPE="text/css" HREF="http://www.datamonkey.org//2007/styles.css">
+		
+	
+ 	</head>
+
+<body background = "/images/aquastripe.gif" bgcolor = "#FFFFFF" >
+
+<DIV align = left style = 'max-width:720px;border-left: solid 3px #FF5223;border-right: solid 3px #FF5223;'>
+<div style = 'text-align:center;background-color:#FF5223;font-size:12px;padding:2px;'>
+
+<a href = '/dataupload.php' class = 'UPLOAD'>Analyze your data</a>
+<a href = 'http://www.datamonkey.org' class = 'NAVBAR'>Home</a>
+<a href = '/help/index.php' class = 'NAVBAR'>Help</a>
+<a href = '/help/citations.php' class = 'NAVBAR'>Citations</a>
+<a href = '/cgi-bin/datamonkey/mpiJobStatus.pl' class = 'NAVBAR'>Job Queue</a>
+<a href = '/2007/usage.php' class = 'NAVBAR'>Stats</a>
+<a href = 'http://hyphy.ucsd.edu' class = 'NAVBAR'>HyPhy package</a>
+<!--<a href = 'http://www.datamonkeys.org' class = 'NAVBAR'>Datamonkeys webcomic</a>-->
+
+</div>
+<img src = 'http://www.datamonkey.org//images/datamonkey_title.png' width = '720' height = '144' border = '0'  alt = 'Navigation Banner'>
+
+<H1 CLASS='SuccessCap'>MEME analysis results</H1><DIV CLASS='RepClassCT'><b>Reports</b> <a href='http://datamonkey.org/cgi-bin/datamonkey/slacreport.pl?file=upload.86275733878210.1&task=12&format=0'>[HTML]</a> <a href='http://datamonkey.org/cgi-bin/datamonkey/slacreport.pl?file=upload.86275733878210.1&task=12&format=1' target = '_blank'>[CSV]</a> <a href='http://datamonkey.org/cgi-bin/datamonkey/slacreport.pl?file=upload.86275733878210.1&task=12&format=2'>[Plots]</a></DIV><DIV Class='RepClassBox'><b>Job ID:</b>upload.86275733878210.1 <a class = 'InfoLink' href='http://datamonkey.org/cgi-bin/datamonkey/jobStatus.pl?file=upload.86275733878210.1' TARGET = '_top'>[Information:Other analyses]</a></DIV><DIV CLASS='RepClassSM'><b>Data summary</b><p><b>76</b> sequences with <b>1</b> partition<DIV CLASS = 'ErrorTagSM'>These sequences have not been screened for recombination. Selection analyses of alignments with recombinants in them using a single tree may generate <u>misleading</u> results.</DIV><br clear=all><span style = 'font-size: x-small; margin-left:10px;'>Partition <b>1</b>: 748 codons 0 subs/site</span>
+</DIV><DIV CLASS='RepClassSM'><form action='http://datamonkey.org/cgi-bin/datamonkey/slacpvalue.pl' method = 'GET'>Found <b>15</b> sites with evidence of episodic diversifying selection (<input type = 'hidden' name = 'file' value = 'upload.86275733878210.1'><input type = 'text' name = 'pvalue' value = '0.1' size = '5'> significance level <input type = 'hidden' value = '12' name = 'task'><input type = 'submit' value = 'Retabulate'>)<p><DIV CLASS = 'HelpfulTips'>This summary table reports the distribution of synonymous (&alpha;) and non-synonymous (&beta;) substitution rates over <b>sites</b> inferred by the MEME model, where the proportion of branches with &beta;&gt;&alpha; is significantly greater than 0. p-value is derived using a mixture of &chi;<sup>2</sup> distributions, and q-values are obtained using <a href = 'http://en.wikipedia.org/wiki/False_discovery_rate#Independent_tests' class = 'REFERENCE'>Simes' procedure</a>, which controls the false discovery rate under the strict neutral null (likely to be conservative).</DIV><DIV CLASS='RepClassSM'>
+<TABLE BORDER = '0'><TR CLASS='TRReport' style = 'font-size:small'><td>Codon</td>
+<td>&alpha;</td>
+<td>&beta;<sup>-</sup></td>
+<td>Pr[&beta;=&beta;<sup>-</sup>]</td>
+<td>&beta;<sup>+</sup></td>
+<td>Pr[&beta;=&beta;<sup>+</sup>]</td>
+<td>p-value</td>
+<td>q-value</td>
+<td>Branch-site information</td></tr>
+
+<tr CLASS='TRReport1' style = 'font-size:x-small'>
+<td>41
+</td>
+
+<td>1.20741
+</td>
+
+<td>0.0397245
+</td>
+
+<td>0.989474
+</td>
+
+<td>635.373
+</td>
+
+<td>0.0105257
+</td>
+
+<td>0.099354
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-41-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport2' style = 'font-size:x-small'>
+<td>127
+</td>
+
+<td>0.106175
+</td>
+
+<td>0.106175
+</td>
+
+<td>0.807918
+</td>
+
+<td>11.3208
+</td>
+
+<td>0.192082
+</td>
+
+<td>0.000140501
+</td>
+
+<td>0.105095
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-127-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport1' style = 'font-size:x-small'>
+<td>137
+</td>
+
+<td>0.255796
+</td>
+
+<td>0
+</td>
+
+<td>0.816333
+</td>
+
+<td>2.37114
+</td>
+
+<td>0.183667
+</td>
+
+<td>0.00956078
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-137-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport2' style = 'font-size:x-small'>
+<td>142
+</td>
+
+<td>0.602736
+</td>
+
+<td>0
+</td>
+
+<td>0.79238
+</td>
+
+<td>4.48423
+</td>
+
+<td>0.20762
+</td>
+
+<td>0.0224615
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-142-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport1' style = 'font-size:x-small'>
+<td>188
+</td>
+
+<td>0
+</td>
+
+<td>0
+</td>
+
+<td>0.991973
+</td>
+
+<td>168.006
+</td>
+
+<td>0.00802652
+</td>
+
+<td>0.0202365
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-188-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport2' style = 'font-size:x-small'>
+<td>203
+</td>
+
+<td>0.167686
+</td>
+
+<td>0.167686
+</td>
+
+<td>0.953721
+</td>
+
+<td>23.5111
+</td>
+
+<td>0.0462787
+</td>
+
+<td>0.00801854
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-203-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport1' style = 'font-size:x-small'>
+<td>228
+</td>
+
+<td>0.605317
+</td>
+
+<td>0
+</td>
+
+<td>0.780735
+</td>
+
+<td>4.15997
+</td>
+
+<td>0.219265
+</td>
+
+<td>0.0850387
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-228-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport2' style = 'font-size:x-small'>
+<td>397
+</td>
+
+<td>0.283179
+</td>
+
+<td>0
+</td>
+
+<td>0.916801
+</td>
+
+<td>5.88983
+</td>
+
+<td>0.083199
+</td>
+
+<td>0.00697208
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-397-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport1' style = 'font-size:x-small'>
+<td>410
+</td>
+
+<td>0.213768
+</td>
+
+<td>0.115516
+</td>
+
+<td>0.908743
+</td>
+
+<td>3.68335
+</td>
+
+<td>0.0912569
+</td>
+
+<td>0.0684059
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-410-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport2' style = 'font-size:x-small'>
+<td>523
+</td>
+
+<td>0.326231
+</td>
+
+<td>0.0258705
+</td>
+
+<td>0.971569
+</td>
+
+<td>157.782
+</td>
+
+<td>0.0284313
+</td>
+
+<td>0.000991858
+</td>
+
+<td>0.370955
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-523-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport1' style = 'font-size:x-small'>
+<td>557
+</td>
+
+<td>1.54998
+</td>
+
+<td>0.0198717
+</td>
+
+<td>0.969522
+</td>
+
+<td>51.6833
+</td>
+
+<td>0.0304779
+</td>
+
+<td>0.0105909
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-557-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport2' style = 'font-size:x-small'>
+<td>563
+</td>
+
+<td>0.730248
+</td>
+
+<td>0.0301536
+</td>
+
+<td>0.953154
+</td>
+
+<td>17.1919
+</td>
+
+<td>0.046846
+</td>
+
+<td>0.0294258
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-563-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport1' style = 'font-size:x-small'>
+<td>575
+</td>
+
+<td>0.293732
+</td>
+
+<td>0
+</td>
+
+<td>0.989976
+</td>
+
+<td>76.7767
+</td>
+
+<td>0.010024
+</td>
+
+<td>0.0510452
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-575-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport2' style = 'font-size:x-small'>
+<td>621
+</td>
+
+<td>0.864536
+</td>
+
+<td>0
+</td>
+
+<td>0.990453
+</td>
+
+<td>134.39
+</td>
+
+<td>0.00954743
+</td>
+
+<td>0.0485556
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-621-0&mode=0'>[Display]</a></TD></tr>
+
+<tr CLASS='TRReport1' style = 'font-size:x-small'>
+<td>747
+</td>
+
+<td>1.58841
+</td>
+
+<td>0.0216592
+</td>
+
+<td>0.957627
+</td>
+
+<td>10.437
+</td>
+
+<td>0.0423734
+</td>
+
+<td>0.0932772
+</td>
+
+<td>1
+</td>
+<TD style = 'font-size: 10px;'><a href='http://datamonkey.org/cgi-bin/datamonkey/wrapHyPhyBF.pl?file=meme_plotter&arguments=upload.86275733878210.1-747-0&mode=0'>[Display]</a></TD></tr>
+</TABLE></DIV>
+</form></DIV><DIV Class='DateStamp'>Page generated on<i> 2016/3/15 14:39 GMT</i></DIV>	<div style = 'text-align:right;background-color:#FF5223;font-size:12px;padding-right:5px;'><i>UCSD Viral Evolution Group 2004-2016</i></div>
+</div>
+<!-- Piwik --> 
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://datamonkey.org/piwik/" : "http://datamonkey.org/piwik/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 6);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://datamonkey.org/piwik/piwik.php?idsite=6" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
+</body>
+</html>
